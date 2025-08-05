81e568777290bc19836a55bb0bd2b743f7be2b90
walkerNegrão <walker.esteves.negrao@gmail.com>
2025-08-05 14:09:10 -0300

    Atualização

--- exercicios/hashtag/exerc-2/main.py
+++
@@ -1,7 +0,0 @@
-# Coca -> Código: BEB1300543<br>
-# Pepsi -> Código: BEB1300545<br>
-# Vinho Primitivo Lucarelli -> Código: BAC1546001<br>
-# Vodka Smirnoff -> Código: BAC17675002<br>
-
-tipo_de_bebida = input("Informe o código da bebida: ")
-print("BAC" in tipo_de_bebida)
