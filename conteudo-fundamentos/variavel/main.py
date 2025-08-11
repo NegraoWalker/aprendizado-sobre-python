7070fa435a552720edc1423a03dbf1a7ab742dc1
walkerNegrão <walker.esteves.negrao@gmail.com>
2025-08-10 21:05:46 -0300

    Atualização

---
+++ conteudo-fundamentos/variavel/main.py
@@ -0,0 +1,7 @@
+nome = "Walker"
+sobrenome = "Negrão"
+idade = 32
+esta_trabalhando_no_momento = True
+salario = 4444.99
+
+print(f"Meu nome é {nome} {sobrenome} tenho {idade} anos de idade, estou trabalhando: {esta_trabalhando_no_momento} e tenho um salário de R${salario}")
